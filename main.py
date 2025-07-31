e7a77a3392f5e6048339a8cd265f2e9ed309f7b3
Pradeep R <130227965+pradeep583@users.noreply.github.com>
2025-07-31 22:11:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import numpy as np
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import os
 
-model = tf.keras.models.load_model('model/my_model.h5')
-
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-
-
-with open('model/model.tflite', 'wb') as f:
-    f.write(tflite_model)
-
-interpreter = tf.lite.Interpreter(model_path="model/model.tflite")
+interpreter = tflite.Interpreter(model_path="model/model.tflite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
@@ -30,17 +21,15 @@ le = LabelEncoder()
 le.fit(classes)
 
 def getPrediction(filename):
-    SIZE = 128 
+    SIZE = 128
     img_path = os.path.join("static", filename)
     img = Image.open(img_path).resize((SIZE, SIZE))
     img = np.asarray(img, dtype=np.float32) / 255.0
     img = np.expand_dims(img, axis=0)
 
-    # Set input and run inference
     interpreter.set_tensor(input_details[0]['index'], img)
     interpreter.invoke()
 
-    # Get predictions
     output_data = interpreter.get_tensor(output_details[0]['index'])
     predicted_index = np.argmax(output_data)
     return le.inverse_transform([predicted_index])[0]
