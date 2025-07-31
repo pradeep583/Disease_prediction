d08eee25e1e6b781536a1ce93915ca473dc1db7d
Pradeep R <130227965+pradeep583@users.noreply.github.com>
2025-07-31 23:21:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,50 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, after_this_request
 from werkzeug.utils import secure_filename
 from main import getPrediction
-
-from flask import flash
 import os
 
-#Save images to the 'static' folder as Flask serves images from this directory
 UPLOAD_FOLDER = 'static'
 
-#Create an app object using the Flask class. 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 app.secret_key = "secret key"
-#Define the upload folder to save images uploaded by the user. 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def index():
     return render_template('client.html')
 
-
 @app.route('/', methods=['POST'])
 def submit_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No file selected for uploading')
+        return redirect(request.url)
 
-           
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
+    if file:
+        filename = secure_filename(file.filename)
+        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-            label = getPrediction(filename)
-            # Flash the prediction and file path
-            flash('Prediction: ')
-            flash(label)
-            flash(file_path)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
 
-            return render_template('client.html', prediction=label, image=file_path)
-            
-        else:
-            flash('Allowed file types are png, jpg, jpeg')
-            return redirect(request.url)
+        label = getPrediction(filename)
 
+        @after_this_request
+        def remove_file(response):
+            try:
+                os.remove(file_path)
+            except Exception as e:
+                app.logger.error("Error deleting file: %s", e)
+            return response
 
+        return render_template('client.html', prediction=label, image=file_path)
+
+    flash('Allowed file types are png, jpg, jpeg')
+    return redirect(request.url)
 
 if __name__ == "__main__":
     app.run()
